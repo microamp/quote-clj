c0c0313b7459b5e76aff797a0cd531eaf3972c55
han semaj <sangho.nah@gmail.com>
2014-06-09 23:25:49 +1200

    Basic setup

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject quote-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "(quote \"clojure\")"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.3.0"]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "quote-clj.jar"
+  :main quote-clj.core
+  :profiles {:dev {:main quote-clj.core/-dev-main}})
--- src/quote_clj/core.clj
+++ src/quote_clj/core.clj
@@ -1,6 +1,34 @@
-(ns quote-clj.core)
+(ns quote-clj.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [compojure.core :refer [defroutes ANY GET]]
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn index [hdlr]
+  {:status 200
+   :headers {}
+   :body "hello, world"})
+
+(defroutes routes
+  "Defines routes"
+  (GET "/" [] index)
+  (ANY "/request" [] handle-dump)
+  (not-found "404"))
+
+(def app
+  "Defines middleware stack"
+  (wrap-file-info
+   (wrap-resource
+    (wrap-params
+     routes)
+    "static")))
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
