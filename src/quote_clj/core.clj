d024821b3d67b96bb5d171dcc84ccc50040f5b68
han semaj <sangho.nah@gmail.com>
2014-06-10 22:41:06 +1200

    Define basic database structure

--- src/quote_clj/core.clj
+++ src/quote_clj/core.clj
@@ -6,7 +6,54 @@
             [ring.middleware.file-info :refer [wrap-file-info]]
             [compojure.core :refer [defroutes ANY GET]]
             [compojure.route :refer [not-found]]
-            [ring.handler.dump :refer [handle-dump]]))
+            [ring.handler.dump :refer [handle-dump]]
+            [clojure.java.jdbc :as jdbc]))
+
+(def db (or (System/getenv "DATABASE_URL")
+            "jdbc:postgresql://localhost/quote_clj?user=postgres"))
+
+(defn create-table []
+  (jdbc/execute!
+   db
+   ["CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""])
+  (jdbc/execute!
+   db
+   [(str "CREATE TABLE IF NOT EXISTS quote ("
+         "id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), "
+         "quote TEXT NOT NULL, "
+         "by TEXT NOT NULL, "
+         "source TEXT, "
+         "link TEXT, "
+         "added TIMESTAMPTZ NOT NULL DEFAULT now()"
+         ")")]))
+
+(defn delete-table []
+  (jdbc/execute!
+   db
+   [(str "DROP TABLE IF EXISTS quote")]))
+
+(defn create-quote [quote by source link]
+  (:id (first (jdbc/query
+               db
+               [(str "INSERT INTO quote (quote, by, source, link) "
+                     "VALUES (?, ?, ?, ?) "
+                     "RETURNING id")
+                quote by source link]))))
+
+(defn read-quotes []
+  (jdbc/query
+   db
+   [(str "SELECT quote, by, source, link, added "
+         "FROM quote "
+         "ORDER BY added")]))
+
+(defn read-quote-by-id [id]
+  (first (jdbc/query
+          db
+          [(str "SELECT quote, by, source, link, added "
+                "FROM quote "
+                "WHERE id = ?")
+           id])))
 
 (defn index [hdlr]
   {:status 200
@@ -31,4 +78,11 @@
   (jetty/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main [port]
+  (delete-table)
+  (create-table)
+  (println (read-quote-by-id
+            (create-quote "Developers know the benefits of everything and trade-offs of nothing."
+                          "Rich Hickey"
+                          "Simplicity Matters"
+                          "http://goo.gl/fPx6Mr")))
   (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
